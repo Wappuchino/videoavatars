c34dfe4a17d5454a0af0cad0cd99e987db782873
Walter Han <wappuchino@gmail.com>
2022-03-27 22:06:13 -0500

    Clarified python version to call

--- run_step3.sh
+++ run_step3.sh
@@ -12,4 +12,4 @@ if [[ $SUBJ = *"female"* ]]; then
   MODEL='--model vendor/smpl/models/basicModel_f_lbs_10_207_0_v1.0.0.pkl'
 fi
 
-python step3_texture.py $SUBJ/consensus.pkl  $SUBJ/camera.pkl $SUBJ/$(basename $SUBJ).mp4 $SUBJ/reconstructed_poses.hdf5 $SUBJ/masks.hdf5 $OUT/tex-$(basename $SUBJ).jpg $MODEL ${@:3}
+python2 step3_texture.py $SUBJ/consensus.pkl  $SUBJ/camera.pkl $SUBJ/$(basename $SUBJ).mp4 $SUBJ/reconstructed_poses.hdf5 $SUBJ/masks.hdf5 $OUT/tex-$(basename $SUBJ).jpg $MODEL ${@:3}
